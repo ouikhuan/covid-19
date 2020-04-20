11eef45996efc2ab5f7edafa4af49807801c5c6d
tavolafourcade <tavolafourcade@gmail.com>
2020-04-20 17:16:12 -0500

    Added the background animation funcionality, also I started the Main Search section, the search box is already complete

--- src/components/main-area/main-area.component.jsx
+++ src/components/main-area/main-area.component.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const MainArea = ()=>(
-    <div className='main-area ba1 b--solid b--dark-gray'></div>
+    <div className='main-area ba1 b--solid b--dark-gray'>
+        <div className="tc b main_title ma1">Title</div>
+
+        {/* Input form */}
+        <div className="search_container tc center">
+            <div className=' pa2 ma2 center search_small_container'>
+                <input className="" type="text" placeholder="Enter Country"/>
+                <i className='ml2 fa fa-search'></i>
+            </div>
+        </div>
+        
+    </div>
 )
 
 export default MainArea;
