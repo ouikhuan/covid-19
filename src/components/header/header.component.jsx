7a56e709a2bba4cd81d4fd8929704bf615c98ad1
tavolafourcade <tavolafourcade@gmail.com>
2020-04-22 12:28:12 -0500

    Added header formatting

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import './header.styles.scss';
 
+
 const Header = () => (
     <header id="header" className='overflow-hidden'>
-        <h1 className="title tc">COVID-19 TRACKER</h1>
+        <h1 className="f1 tc">COVID-19 TRACKER</h1>
         <div className='options fr flex mr4'>
-            <Link className='option pa2' to='/' >Informations</Link>
-            <Link className='option pa2' to='/map' >Global map</Link>
-            <Link className='option pa2' to='/about' >About us</Link>
+            <Link className='option pa2 f2 ' to='/' >Information</Link>
+            <Link className='option pa2 f2' to='/map' >Global impact</Link>
+            <Link className='option pa2 f2' to='/about' >About us</Link>
         </div>
     </header>
 );
