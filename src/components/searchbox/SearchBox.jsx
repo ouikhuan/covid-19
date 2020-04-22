9d648dc37f4ba5cedec8f7c5d9e2692cbbc6bae8
tavolafourcade <tavolafourcade@gmail.com>
2020-04-22 01:02:20 -0500

    Added full funcionality to the Searchbox and interaction with Cards and Title

--- src/components/searchbox/SearchBox.jsx
+++ src/components/searchbox/SearchBox.jsx
@@ -4,7 +4,7 @@ const SearchBox = ({searchChange}) => {
     return(
         <div className="search_container tc center">
             <div className=' pa2 ma2 center search_small_container'>
-                <input className="" type="text" placeholder="Enter Country" onChange={searchChange}/>
+                <input className="" type="text" placeholder="Enter Country" onChange={searchChange} />
                 <i className='ml2 fa fa-search'></i>
             </div>
 
