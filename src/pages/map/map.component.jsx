00835ef7a597c8c1cdaea4c5fcf7aedb8ac4a796
Wayne Wang <iverson0217@gmail.com>
2020-04-23 21:12:02 +0800

    finished global map implementation

--- src/pages/map/map.component.jsx
+++ src/pages/map/map.component.jsx
@@ -1,7 +1,106 @@
 import React from 'react';
-const MapPage = ()=>(
-    <div className='map'>
-        <h1>Map</h1>
-    </div>
-)
+import { Map, Marker, Popup, TileLayer,CircleMarker } from "react-leaflet";
+import axios from 'axios';
+import {getScaleAndColorByNumber} from '../../utils/map.utils';
+
+import 'leaflet/dist/leaflet.css';
+import './map.styles.scss';
+
+class MapPage extends React.Component{
+
+    constructor(){
+        super();
+        this.state = {
+            mapSettings:{
+                center: [0,0],
+                zoom: 3,
+            },
+            activeCountry:null,
+            mappingCountries:[]
+        }
+    }
+
+    componentDidMount(){
+        this.fetchMapData();
+    }
+
+    async fetchMapData(){
+        try {
+            const response = await axios.get('https://corona.lmao.ninja/v2/countries');
+            const { data } = response;
+
+            if(data.length){
+
+                this.setState({
+                    mappingCountries: data
+                });
+            }
+
+
+        } catch(e) {
+            console.log(`Failed to fetch mapData: ${e.message}`, e);
+            return;
+        }
+    }
+
+    setActiveCountry = (country) => {
+        this.setState({
+            activeCountry:country
+        });
+    }
+
+
+
+    render(){
+        return (
+        <div className='map'>
+            <Map center={this.state.mapSettings.center} zoom={this.state.mapSettings.zoom}>
+                <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+
+                {this.state.mappingCountries.map((country,index) => {
+
+                    const {scale,color} = getScaleAndColorByNumber(country.cases);
+                    return (
+                        <Marker
+                          key={index}
+                          position={{lat:country.countryInfo.lat, lng: country.countryInfo.long}}
+                          onClick={() => {
+                            this.setActiveCountry(country);
+                          }}
+                        >
+                        <CircleMarker
+                        center={{lat:country.countryInfo.lat, lng: country.countryInfo.long}}
+                        fillColor={color}
+                        opacity='0'
+                        fillOpacity='0.6'
+                        radius={scale} />
+                        </Marker>
+                    )
+                })}
+
+                {this.state.activeCountry && (
+                    <Popup
+                      position={{
+                            lat:this.state.activeCountry.countryInfo.lat,
+                            lng: this.state.activeCountry.countryInfo.long
+                        }}
+                      onClose={() => {
+                        this.setActiveCountry(null);
+                      }}
+                    >
+                      <div>
+                        <h2>{this.state.activeCountry.country}<img className='flag' src={this.state.activeCountry.countryInfo.flag} alt='' /></h2>
+                        <h4>Total cases: {this.state.activeCountry.cases}</h4>
+                        <h4>Total deaths: {this.state.activeCountry.deaths}</h4>
+                        <h4>Total recovered: {this.state.activeCountry.recovered}</h4>
+                      </div>
+                    </Popup>
+                  )}
+            </Map>
+        </div>)
+    }
+}
 export default MapPage;
---
+++ src/utils/map.utils.js
@@ -0,0 +1,47 @@
+export const getScaleAndColorByNumber = (number) => {
+
+    let scale, color;
+    switch (true) {
+        case (number >= 1 && number <= 5000):
+            scale = 5;
+            color = '#ffc34d';
+            break;
+        case (number > 5000 && number <= 10000):
+            scale = 10;
+            color = '#f7ce90';
+            break;
+        case (number > 10000 && number <= 100000):
+            scale = 20;
+            color = '#f58c41';
+            break;
+        case (number > 200000 && number <= 300000):
+            scale = 40;
+            color = '#f58434';
+            break;
+        case (number > 300000 && number <= 400000):
+            scale = 50;
+            color = '#f59029';
+            break;
+        case (number > 400000 && number <= 500000):
+            scale = 60;
+            color = '#fb821b';
+            break;
+        case (number > 500000 && number <= 600000):
+            scale = 70;
+            color = '#fb6e1b';
+            break;
+        case (number >= 600000):
+            scale = 90;
+            color = '#ff6328';
+            break;
+        default:
+            scale=0;
+            color='';
+            break;
+    }
+
+    return {
+        scale,
+        color
+    }
+}
