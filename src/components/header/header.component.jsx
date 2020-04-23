1ab9d1369b56566f28ea34d85f64bac4b8f8b3ae
Wayne Wang <iverson0217@gmail.com>
2020-04-23 17:02:10 +0800

    styling header and right panel properly

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,12 +4,13 @@ import './header.styles.scss';
 
 
 const Header = () => (
-    <header id="header" className='overflow-hidden'>
-        <h1 className="f1 tc">COVID-19 TRACKER</h1>
-        <div className='options fr flex mr4'>
-            <Link className='option pa2 f2 ' to='/' >Information</Link>
-            <Link className='option pa2 f2' to='/map' >Global impact</Link>
-            <Link className='option pa2 f2' to='/about' >About us</Link>
+    <header id="header" className='overflow-hidden flex justify-between items-center'>
+        <h1 className="f1 tc ml3"><i class="fa fa-bar-chart mr2" aria-hidden="true"></i>
+        COVID-19 TRACKER</h1>
+        <div className='options fr flex mr2'>
+            <Link className='option pa2 f4' to='/' >Information</Link>
+            <Link className='option pa2 f4' to='/map' >Global impact</Link>
+            <Link className='option pa2 f4' to='/about' >About us</Link>
         </div>
     </header>
 );
