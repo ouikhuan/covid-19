f229ea13a3803e6f431cbb105ffad95950414d09
Wayne Wang <iverson0217@gmail.com>
2020-04-27 22:49:30 +0800

    make menu responsive

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,14 +4,16 @@ import './header.styles.scss';
 
 
 const Header = () => (
-    <header id="header" className='overflow-hidden flex justify-between items-center'>
+    <header id="header" className='overflow-hidden items-center'>
         <h1 className="f1-l f3-ns tc ml3"><i className="fa fa-bar-chart mr2" aria-hidden="true"></i>
         COVID-19 TRACKER</h1>
-        <div className='options fr flex mr2'>
-            <Link className='option pa2 f4' to='/' >Information</Link>
-            <Link className='option pa2 f4' to='/map' >Global impact</Link>
-            <Link className='option pa2 f4' to='/about' >About us</Link>
-        </div>
+        <input className="menu-btn" type="checkbox" id="menu-btn" />
+        <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
+        <nav className='options mr2'>
+            <li><Link className='option pa2 f4' to='/' >Information</Link></li>
+            <li><Link className='option pa2 f4' to='/map' >Global impact</Link></li>
+            <li><Link className='option pa2 f4' to='/about' >About us</Link></li>
+        </nav>
     </header>
 );
 
